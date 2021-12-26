9cec12331cd14ab845106ce943fc63b8d85048e1
rfjohnson3 <96011569+rfjohnson3@users.noreply.github.com>
2021-12-26 10:01:50 -0500

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -24,12 +24,21 @@ number=100000
 globals-globals()
 print()
 
+timeit.timeit()
+stmt = "expenses.categorize_set_comprehension"()
+setup = '''from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')'''
+number=100000
+globals-globals()
+print()
+
 fix,ax=plt.subplots()
 labels = ('Necessary', 'Food", Unnecessary')
 
 divided_expenses_sum = []
-    for catagory.exps(divided_set_comp):
-        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+for category.exps(divided_set_comp):
+    divided_expenses_sum.append(sum(x.amount for x in category_exps))
 
 ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
 
